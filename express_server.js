48a79e8922e7eb375291901e9e66cb7d267465df
Richard Zhang <lapislacrima@gmail.com>
2016-11-08 21:03:51 +0000

    correctly response to root, urls, urls.json, as well as test case /hello

---
+++ express_server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  res.render("urls_index", {urls: urlDatabase});
+});
+
+app.get("/urls/:short", (req, res) => {
+  let long = urlDatabase.short;
+  res.render("urls_show", {short: long});
+});
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
