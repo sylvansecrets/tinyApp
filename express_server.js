baece02a5fb79cc27c332dc78dcc558c1782e8db
Richard Zhang <lapislacrima@gmail.com>
2016-11-08 21:46:26 +0000

    now requires body-parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true})
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
